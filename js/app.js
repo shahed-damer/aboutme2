f057b22c7c1e99ae60cb9b83c856daf18390ffd2
shahed qerbaa <shaheddamer@gmail.com>
2021-06-07 18:11:21 +0300

    my type

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict'
+let userName =prompt('Whats Your Name');
+alert('Welcome '+userName)
+
+let videoGame =prompt('Do you Think I like Video Games').toLowerCase();
+if (videoGame==='yes') {
+alert('thats awsome '+userName)
+console.log('Great answer')
+}
+
+let footBall =prompt('Do you Think I like FootBall').toLowerCase();
+if (footBall==='yes') {
+alert('madimm '+userName)
+console.log('Not my Type ')
+}
+
+let favColor =prompt('Do you Think My Favorite color is Red').toLowerCase();
+if (favColor==='yes') {
+alert('Your Awezird '+userName)
+console.log('Great answer')
+}
+
+let danceWith =prompt('Do you Think I liKE Dancing').toLowerCase();
+if (danceWith==='yes') {
+alert('Ya Your Amazing '+userName)
+console.log('Great answer')
+}
+
+let favDri =prompt('Do you Think I like Drink Coffie').toLowerCase();
+if (favDri==='yes') {
+alert('Welcome '+userName)
+console.log('Great answer')
+}
